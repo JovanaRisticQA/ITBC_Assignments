3997174a28d83c5160eb5fb0c7c7197ecd9b879b
Jovana <135367233+JovanaRisticQA@users.noreply.github.com>
2023-06-30 19:51:55 +0200

    Assignment3.java

--- home_assignment_12/Assignment3.java
+++ home_assignment_12/Assignment3.java
@@ -2,10 +2,6 @@ package home_assignment_12;
 
 public class Assignment3 {
     public static void main(String[] args) {
-        /*
-            3.Kreirati dva niza i prikazati koji niz ima vise clanova
-            (Ne morate petlju koristiti vec mozete if statement)
-         */
 
             int[] array1 = {2, 4, 6, 8, 10, 12};
             int[] array2 = {364, 528, - 385, 10243};
