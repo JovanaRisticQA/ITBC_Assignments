37438b449804949b01b64d75e4cc008e75e8105d
Jovana <135367233+JovanaRisticQA@users.noreply.github.com>
2023-06-30 19:51:21 +0200

    Assignment2.java

--- home_assignment_12/Assignment2.java
+++ home_assignment_12/Assignment2.java
@@ -2,11 +2,6 @@ package home_assignment_12;
 
 public class Assignment2 {
     public static void main(String[] args) {
-        /*
-            2.Kreirati niz proizvoljne duzine celobrojnih vrednosti
-            i pomnoziti poslednji clan sa ukupnim brojem clanova
-            (Koristiti .length, ne fiksnu vrednost)
-         */
 
             int[] array = {4, 56, 7, 8, -82, 12, 35, 4, 7, 18};
 
